f5b2b410bfd553601a00014a879a929a28db3cb3
Harper Foley <harper.e.foley@gmail.com>
2025-07-30 19:18:28 -0700

    Update HubSpot tracking script to use client component

---
+++ components/HubSpotTracking.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function HubSpotTracking() {
+    return (
+        <script
+            type="text/javascript"
+            id="hs-script-loader"
+            async
+            defer
+            src="//js-na2.hs-scripts.com/243464187.js"
+        />
+    );
+}
